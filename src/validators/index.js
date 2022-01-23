c060989ce3084aa4adabe643cf3051e4298124a0
nikochetov <nikochetov@gmail.com>
2022-01-23 17:40:17 +0300

    added validators barrel

---
+++ src/validators/index.js
@@ -0,0 +1,5 @@
+import feedDoublesValidator from './feedDoublesValidator';
+import formValidator from './rssUrlValidator';
+import containRssValidator from './contain-rss-validator';
+
+export { feedDoublesValidator, formValidator, containRssValidator };
