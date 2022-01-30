163671012b3b93f4ddadc2fe429de366c0b1b6bd
nikochetov <nikochetov@gmail.com>
2022-01-30 17:49:56 +0300

    update parser (not set ids)

--- src/app/parser.js
+++ src/app/parser.js
@@ -3,27 +3,19 @@ const parseXml = (xml) => {
   return parser.parseFromString(xml, 'application/xml');
 };
 
-const parseXmlToObj = (elems) => {
-  return Array.from(elems).reduce((acc, current) => {
-    if (current.childElementCount) {
-      const items = acc.items || [];
-      return ({ ...acc, items: [...items, parseXmlToObj(current.children)] });
-    }
+const parseXmlToObj = (elems) => Array.from(elems).reduce((acc, current) => {
+  if (current.childElementCount) {
+    const items = acc.items || [];
+    return ({ ...acc, items: [...items, parseXmlToObj(current.children)] });
+  }
 
-    return ({ ...acc, [current.tagName]: current.textContent });
-  }, {});
-};
-
-const setIds = (parsedData) => {
-  const itemsWithId = parsedData.items.map((item) => ({ ...item }));
-  return ({ ...parsedData, items: itemsWithId });
-};
+  return ({ ...acc, [current.tagName]: current.textContent });
+}, {});
 
 const getChannel = (html) => html.querySelector('channel');
 
 export default (xmlData) => {
   const htmlData = parseXml(xmlData);
   const channel = getChannel(htmlData);
-  const parsedData = parseXmlToObj(channel.children);
-  return setIds(parsedData);
+  return parseXmlToObj(channel.children);
 };
