4207ece50d55632b335afc25a161192854a5f815
nikochetov <nikochetov@gmail.com>
2022-02-19 19:02:48 +0300

    remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
