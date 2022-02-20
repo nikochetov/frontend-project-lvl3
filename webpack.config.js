eb843b268e311006645acc77594523a35cc412e8
nikochetov <nikochetov@gmail.com>
2022-02-20 23:56:47 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssWebpackPlugin = require('mini-css-extract-plugin');
+const { DefinePlugin } = require('webpack');
 
-// const isProd = () => process.env.NODE_ENV === 'production';
+const isProd = () => process.env.NODE_ENV === 'production';
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -10,6 +14,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    // publicPath: '/',
     assetModuleFilename: 'assets/[hash][ext][query]',
   },
   devServer: {
@@ -19,18 +24,27 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [isProd() ? MiniCssWebpackPlugin.loader : 'style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      // {
+      //   test: /\.html$/i,
+      //   loader: 'html-loader',
+      // },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+      favicon: './src/assets/favicon.ico',
     }),
+    new MiniCssWebpackPlugin(),
     new CleanWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+    }),
   ],
 };
