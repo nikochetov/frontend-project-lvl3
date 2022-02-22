8ac7ea546cccccd1a88deed0e8c89576dbd29f80
nikochetov <nikochetov@gmail.com>
2022-02-22 10:36:08 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import MiniCssWebpackPlugin from 'mini-css-extract-plugin';
 
-// const isProd = () => process.env.NODE_ENV === 'production';
+const isProd = () => process.env.NODE_ENV === 'production';
 
 const webpackConfig = {
   mode: process.env.NODE_ENV || 'development',
-  entry: 'index.js',
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[hash][ext][query]',
   },
   devServer: {
     port: 4300,
@@ -18,14 +22,25 @@ const webpackConfig = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [isProd() ? MiniCssWebpackPlugin.loader : 'style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
+      favicon: './src/assets/favicon.ico',
     }),
+    new MiniCssWebpackPlugin(),
     new CleanWebpackPlugin(),
   ],
 };
