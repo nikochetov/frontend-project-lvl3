2058d624a2b3f8b07dbbbfe666ff9d48e5c76d6c
nikochetov <nikochetov@gmail.com>
2022-02-24 23:41:12 +0300

    update configs

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import i18next from 'i18next';
 import { setLocale } from 'yup';
 import resources from '../locales/index.js';
