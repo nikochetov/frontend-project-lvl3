e24acedf42b0f0a1b762b6eef007058dfb84b386
nikochetov <nikochetov@gmail.com>
2022-02-25 14:46:39 +0300

    init test

--- __tests__/1-app.test.js
+++ __tests__/1-app.test.js
@@ -3,15 +3,21 @@
 import { screen } from '@testing-library/dom';
 // import userEventHelpers from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-
+import path from 'path';
+import fs from 'fs';
+import { URL } from 'url';
 import app from '../src/index.js';
 
-// const getFixture = (filename) => fs.readFileSync(path.join('__fixtures__', filename)).toString();
+const index = path.join('..', 'index.html');
+const initHtml = fs.readFileSync(new URL(index, import.meta.url), 'utf-8');
 
 beforeEach(async () => {
+  document.body.innerHTML = initHtml;
+
   await app();
 });
 
 test('init application', async () => {
-  expect(screen.getByRole('alert')).not.toBeInTheDocument();
+  await expect(screen.getByRole('button', { name: /add/i })).toBeInTheDocument();
+  await expect(screen.getByLabelText('RSS link')).toBeInTheDocument();
 });
