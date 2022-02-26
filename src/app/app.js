f61757c1724a4734ae2a48514b719289ef3adfdc
nikochetov <nikochetov@gmail.com>
2022-02-26 11:23:51 +0300

    remove styles

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/js/dist/modal.js';
 import i18next from 'i18next';
 import { setLocale } from 'yup';
