f22fd396911ae10422f2185677153bd64aab2890
nikochetov <nikochetov@gmail.com>
2022-02-26 13:38:33 +0300

    added role attribute to posts 'more' button

--- src/app/view.js
+++ src/app/view.js
@@ -7,6 +7,7 @@ const createButton = (postId, i18nInstance) => {
   button.setAttribute('data-bs-toggle', 'modal');
   button.setAttribute('data-bs-target', '#rssDetailsModal');
   button.setAttribute('data-postId', postId);
+  button.setAttribute('role', 'button');
   button.classList.add('btn', 'btn-outline-primary', 'ms-1');
   button.textContent = i18nInstance.t('buttons.moreButton');
   return button;
