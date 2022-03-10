da40e5bf1281dc949748eae2776af4ccca26b347
nikochetov <nikochetov@gmail.com>
2022-03-10 13:06:50 +0300

    fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,17 @@
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import { fileURLToPath } from 'url';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const isProd = () => process.env.NODE_ENV === 'production';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    main: path.resolve(__dirname, 'src', 'index.js'),
+    main: './src/index.js',
   },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: './dist',
     clean: true,
   },
   devtool: 'source-map',
